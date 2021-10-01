0733f123781e21ce68b1a54ec39f889aaf962dc1
NickDubDrums <niko.debe@libero.it>
2021-10-01 17:00:49 +0200

    Modal All 1

--- js/modalScript.js
+++ js/modalScript.js
@@ -1,6 +1,7 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target')
-const closeModalButtons = document.querySelectorAll('[data-close-button')
-const overlay = document.getElementById('overlay')
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay1 overlay2 overlay3 overlay4 overlay5 overlay6')
+
 
 
 openModalButtons.forEach(button => {
@@ -12,7 +13,7 @@ openModalButtons.forEach(button => {
 
 closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
-        const modal = button.closest('.modal')
+        const modal = button.closest('#overlay1, #overlay2, #overlay3, #overlay4, #overlay5, #overlay6')
         closeModal(modal)
     })
 })
@@ -29,3 +30,16 @@ function closeModal(modal){
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+
+
+
+/*var openModalButtons = document.querySelectorAll('.open-modal');
+
+openModalButtons.forEach(function(btn){
+    btn.onclick = function(){
+        var modal = btn.getAttribute('data-modal');
+
+        document.getElementById(modal).style.display ='block';
+    };
+});*/
